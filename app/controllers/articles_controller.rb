759c9bee02508e58a656a0c3abf95e0cd01234b1
Ed Salinas <edsalinas7b@yahoo.com>
2022-09-22 19:21:58 +0800

    Permit image on articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,6 +66,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:name, :body)
+      params.require(:article).permit(:name, :body, :image)
     end
 end
